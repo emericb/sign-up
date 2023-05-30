21ea5f8a5da3b007377cd454c75ef775411bff94
Emeric Bous <38536419+emericb@users.noreply.github.com>
2023-05-30 11:55:24 +0200

    add sign-up

---
+++ src/app/sign-up/sign-up.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sign-up',
+  templateUrl: './sign-up.component.html',
+  styleUrls: ['./sign-up.component.css']
+})
+export class SignUpComponent {
+  model: User = new User("","","","");
+
+  constructor() {}
+  onSubmit() : void {
+    console.log(this.model);
+  }
+}
+
+export class User {
+  constructor(
+    public email: string,
+    public firstname: string,
+    public lastname: string,
+    public password: string
+  ) {}
+}
